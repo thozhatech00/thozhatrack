b1c2d08b88215f19836d051297269efaee6b912b
thozhatech00 <sathishsrm94@gmail.com>
2026-06-24 22:12:54 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+
+const repoName = "thozhatrack";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
